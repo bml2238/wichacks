0a2dff870727ec27738d4d3717438b7d8666b757
Daniyah Wong <dkw8800@rit.edu>
2020-02-29 15:59:35 -0500

    added fields to item

--- .idea/src/java/Item.java
+++ .idea/src/java/Item.java
@@ -2,5 +2,6 @@ public abstract class Item {
     /** item stat effects */
     private int selfEffect; //self esteem
     private int repEffect;
-
+    private String description;
+    private String imglink;
 }
