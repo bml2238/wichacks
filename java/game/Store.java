d0246e0b583df3c7ac4e7bfd99e2e3d2bacde5d1
Kayla Hodgson <kth5870@rit.edu>
2020-02-29 17:44:37 -0500

    removed fields

--- java/game/Store.java
+++ java/game/Store.java
@@ -1,13 +1,6 @@
 package game;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Random;
-
 public class Store {
-
-    private int effect;
 
     public void purchaseMakeup(Item item){
 
