4b2b57ddf62848ee5a94ac81909dff6bc7aef08c
Kayla Hodgson <kth5870@rit.edu>
2020-02-29 17:31:19 -0500

    adding purchase methods

--- .idea/src/java/game/Store.java
+++ .idea/src/java/game/Store.java
@@ -1,20 +1,20 @@
 package game;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Random;
 
 public class Store {
 
     private int effect;
     // just random set to help determine effect on player
-    private HashMap<String, Integer> clothing = new HashMap<>();
-    private Random random;
 
-    public void setClothing(HashSet<String> clothing) { // need to differentiate between which is which i guess
-        for (String clothes : clothing){
-            this.clothing.put(clothes, (int) Math.random());
-        }
+    public void purchaseMakeup(Item item){
+
+    }
+
+    public void purchaseMenstrualProducts(Item item){
+
+    }
+
+    public void purchaseClothing(Item item){
+
     }
 
 }
