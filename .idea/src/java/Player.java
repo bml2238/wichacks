97622a8431fd3bffcb1f3f85c3f53dccf487ffa4
Brie Lindberg <bml2238@rit.edu>
2020-02-29 15:37:12 -0500

    working on it

--- .idea/src/java/Player.java
+++ .idea/src/java/Player.java
@@ -1,2 +1,26 @@
 public class Player {
+
+    /** player variables */
+    private String name;
+    private int selfEsteem;
+    private double income;
+    private double age;
+    private int repuation;
+    private double money;
+    public Race race;
+    public Orientation orien;
+    public Class econ; // "economic status" cause class doesn't work
+
+    /** categories */
+    public enum Race {WHITE, BLACK, ASIAN, HISPANIC}
+    public enum Orientation {STRAIGHT, GAY}
+    public enum Class{LOWER, MIDDLE, UPPER}
+
+    /** initialize player */
+    public Player() {
+        race = Race.WHITE;
+
+    }
+
+
 }
