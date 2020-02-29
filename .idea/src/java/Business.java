be9bfc5444a947ca9bede1359a2170feeb87e0ab
Daniyah Wong <dkw8800@rit.edu>
2020-02-29 16:13:58 -0500

    adding to business

--- .idea/src/java/Business.java
+++ .idea/src/java/Business.java
@@ -1,5 +1,15 @@
 public class Business {
     private int employees;
     private int businessIncome;
+    public int level;
+    public int exp;
 
+    public enum action
+    {
+        HIRE,
+        FIRE,
+        ADVERTISE,
+        VIEW,
+        GETINVESTMENTS
+    }
 }
