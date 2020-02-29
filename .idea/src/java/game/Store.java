dfb872bca9452ca8a25752a3e4537f15daec5e3b
Kayla Hodgson <kth5870@rit.edu>
2020-02-29 17:39:23 -0500

    store

--- .idea/src/java/game/Store.java
+++ .idea/src/java/game/Store.java
@@ -3,7 +3,6 @@ package game;
 public class Store {
 
     private int effect;
-    // just random set to help determine effect on player
 
     public void purchaseMakeup(Item item){
 
@@ -14,6 +13,10 @@ public class Store {
     }
 
     public void purchaseClothing(Item item){
+
+    }
+
+    public void purchaseMedicine(Item item){
 
     }
 
