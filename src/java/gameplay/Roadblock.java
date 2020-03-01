4aee913d3569cf3bc71764da1e2c1d33bbb8e11b
Lori Liu <lyl2505@rit.edu>
2020-03-01 10:07:58 -0500

    Roadblock issues

--- src/java/gameplay/Roadblock.java
+++ src/java/gameplay/Roadblock.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /**
  * in the game progression, these are guaranteed events
  */
-public class RoadBlock {
+public class Roadblock {
 
     /** a roadblock is a timed event */
     private String name;
@@ -26,11 +26,11 @@ public class RoadBlock {
     }
 
     /** list of roadblocks */
-    private ArrayList<RoadBlock> roadblocks = new ArrayList<>();
-    private ArrayList<RoadBlock> intervalRoadBlock = new ArrayList<>();
+    private ArrayList<Roadblock> roadblocks = new ArrayList<>();
+    private ArrayList<Roadblock> intervalRoadBlock = new ArrayList<>();
 
     /** roadblock that will occur when reaching a certain time */
-    public RoadBlock(String name, int timeTriggered, int penalty, Type type) {
+    public Roadblock(String name, int timeTriggered, int penalty, Type type) {
         this.name = name;
         this.timeTriggered = timeTriggered;
         //this.event = e;
@@ -38,13 +38,13 @@ public class RoadBlock {
     }
 
     /** creates array of roadblocks */
-    public ArrayList<RoadBlock> createRoadblocks() {
+    public ArrayList<Roadblock> createRoadblocks() {
         /** $$$$$$$$$$$$$$$$$ NO_CHILDREN $$$$$$$$$$$$$$$$$$$$$$*/
-        final RoadBlock NO_CHILDREN = new RoadBlock("NO_CHILDREN", 228, -20, Type.RESPECT);
+        final Roadblock NO_CHILDREN = new Roadblock("NO_CHILDREN", 228, -20, Type.RESPECT);
         roadblocks.add(NO_CHILDREN);
 
         /** $$$$$$$$$$$$$$$$$$$$ BEAUTY STANDARDS $$$$$$$$$$$$$$ */
-        final RoadBlock BEAUTY_STANDARDS = new RoadBlock("BEAUTY_STANDARDS", 228, -30, Type.SELF_ESTEEM);
+        final Roadblock BEAUTY_STANDARDS = new Roadblock("BEAUTY_STANDARDS", 228, -30, Type.SELF_ESTEEM);
         roadblocks.add(BEAUTY_STANDARDS);
 
         return roadblocks;
