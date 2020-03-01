9080843b3f3ebf39672a2e13cbc59560512e92ab
Kayla Hodgson <kth5870@rit.edu>
2020-03-01 00:41:01 -0500

    fixing roadblock

--- src/java/gameplay/Roadblock.java
+++ src/java/gameplay/Roadblock.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /**
  * in the game progression, these are guaranteed events
  */
-public class Roadblock {
+public class RoadBlock {
 
     /** a roadblock is a timed event */
     private String name;
@@ -26,11 +26,11 @@ public class Roadblock {
     }
 
     /** list of roadblocks */
-    private ArrayList<Roadblock> roadblocks = new ArrayList<>();
-    private ArrayList<Roadblock> intervalRoadblock = new ArrayList();
+    private ArrayList<RoadBlock> roadblocks = new ArrayList<>();
+    private ArrayList<RoadBlock> intervalRoadblock = new ArrayList();
 
     /** roadblock that will occur when reaching a certain time */
-    private Roadblock(String name, int timeTriggered, int penalty, Type type) {
+    private RoadBlock(String name, int timeTriggered, int penalty, Type type) {
         this.name = name;
         this.timeTriggered = timeTriggered;
         //this.event = e;
@@ -38,27 +38,27 @@ public class Roadblock {
     }
 
     /** roadblock that will occur after a certain interval */
-    private void intervalRoadBlock(String name, int timeInterval, Type type, Effect effect) {
+    private intervalRoadBlock(String name, int timeInterval, Type type, Effect effect) {
         this.name = name;
         this.timeInterval = timeInterval;
         this.type = type;
     }
 
     /** creates array of roadblocks */
-    public ArrayList<Roadblock> createRoadblocks() {
+    public ArrayList<RoadBlock> createRoadblocks() {
         /** $$$$$$$$$$$$$$$$$ NO_CHILDREN $$$$$$$$$$$$$$$$$$$$$$*/
-        final Roadblock NO_CHILDREN = new Roadblock("NO_CHILDREN", 228, -20, Type.RESPECT);
+        final RoadBlock NO_CHILDREN = new RoadBlock("NO_CHILDREN", 228, -20, Type.RESPECT);
         roadblocks.add(NO_CHILDREN);
 
         /** $$$$$$$$$$$$$$$$$$$$ BEAUTY STANDARDS $$$$$$$$$$$$$$ */
-        final Roadblock BEAUTY_STANDARDS = new Roadblock("BEAUTY_STANDARDS", 228, -30, Type.SELF_ESTEEM);
+        final RoadBlock BEAUTY_STANDARDS = new RoadBlock("BEAUTY_STANDARDS", 228, -30, Type.SELF_ESTEEM);
         roadblocks.add(BEAUTY_STANDARDS);
 
         return roadblocks;
     }
 
     /** creates array of intervaled roadblocks */
-    public ArrayList<intervalRoadBlock> intervalRoadBlock{
+    public ArrayList<RoadBlock> intervalRoadBlock{
         /** $$$$$$$$$$$$$$$$$ MENSTRUAL CYCLE $$$$$$$$$$$$$$$$$$$$$ */
         final intervalRoadBlock MENSTRUAL_CYCLE = new intervalRoadBlock("MENSTRUAL CYCLE", 1, Type.SELF_ESTEEM);
         intervalRoadblock.add(MENSTRUAL_CYCLE);
