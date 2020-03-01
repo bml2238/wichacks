f43e80bce1528d72298369bcf1dddf00bd9b8592
Lori Liu <lyl2505@rit.edu>
2020-03-01 12:02:41 -0500

    Added concept of time

--- src/java/game/main.java
+++ src/java/game/main.java
@@ -21,8 +21,13 @@ public class main {
         Game game = new Game(player);
 
         int time = 0;
+        int death = 85 * 12;
+        int year = 2000;
+        int yearInc = 5;
+        String month;
         Scanner scan = new Scanner(System.in);
         System.out.print("What is your name? ");
+
         String name = scan.nextLine();
 
         System.out.println("----------------------------------------------------" + "\n" +
@@ -38,15 +43,60 @@ public class main {
                           "-----------------------------------------------------");
         ArrayList<Event> events = Event.createEvents(player); //remember to check events.txt for when they trigger!
 
-        time = game.passTime();
+        while (time != death) {
+            time = game.passTime();
+            if (yearInc % 12 == 0) {
+                month = "December";
+            }
+            else if (yearInc % 11 == 0) {
+                month = "November";
+            }
+            else if (yearInc % 10 == 0) {
+                month = "October";
+            }
+            else if (yearInc % 9 == 0) {
+                month = "September";
+            }
+            else if (yearInc % 8 == 0) {
+                month = "August";
+            }
+            else if (yearInc % 7 == 0) {
+                month = "July";
+            }
+            else if (yearInc % 6 == 0) {
+                month = "June";
+            }
+            else if (yearInc % 5 == 0) {
+                month = "May";
+            }
+            else if (yearInc % 4 == 0) {
+                month = "April";
+            }
+            else if (yearInc % 3 == 0) {
+                month = "March";
+            }
+            else if (yearInc % 2 == 0) {
+                month = "February";
+            }
+            else {
+                month = "January";
+            }
+            System.out.println("Day: " + month + " " + year);
+            yearInc += 1;
+            if (month == "December") {
+                year += 1;
+                yearInc = 1;
+            }
 
-        /** if player has a condo or apartment, every month, they will charge a fee */
-        if (player.getProperty() == "Condo" || player.getProperty() == "Apartment") {
-        }
+            /** if player has a condo or apartment, every month, they will charge a fee */
+            if (player.getProperty() == "Condo" || player.getProperty() == "Apartment") {
+            }
 
-        /** after a certain time has passed, then your status will chance */
-        if (time == 228) {
+            /** after a certain time has passed, then your status will chance */
+            if (time == 228) {
+            }
         }
+        System.out.println("You lived a life, and then you died.");
 
 
 
